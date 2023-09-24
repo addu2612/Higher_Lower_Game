afb5c3474fa3b370a13610063c774c1491412cae
Arnav Panicker <117341727+addu2612@users.noreply.github.com>
2023-09-24 13:45:40 +0530

    Add files via upload

---
+++ higher_lower.py
@@ -0,0 +1,321 @@
+clubs =[
+
+    {
+        "name": "Real Madrid",
+        "year_founded": "1902",
+        "city": "Madrid"
+    },
+    {
+        "name": "Barcelona",
+        "year_founded": "1899",
+        "city": "Barcelona"
+    },
+    {
+        "name": "Bayern Munich",
+        "year_founded": "1900",
+        "city": "Munich"
+    },
+    {
+        "name": "Manchester United",
+        "year_founded": "1878",
+        "city": "Manchester"
+    },
+    {
+        "name": "Liverpool",
+        "year_founded": "1892",
+        "city": "Liverpool"
+    },
+    {
+        "name": "Juventus",
+        "year_founded": "1897",
+        "city": "Turin"
+    },
+    {
+        "name": "AC Milan",
+        "year_founded": "1899",
+        "city": "Milan"
+    },
+    {
+        "name": "Inter Milan",
+        "year_founded": "1908",
+        "city": "Milan"
+    },
+    {
+        "name": "Chelsea",
+        "year_founded": "1905",
+        "city": "London"
+    },
+    {
+        "name": "Arsenal",
+        "year_founded": "1886",
+        "city": "London"
+    },
+    {
+        "name": "Tottenham Hotspur",
+        "year_founded": "1882",
+        "city": "London"
+    },
+    {
+        "name": "Manchester City",
+        "year_founded": "1880",
+        "city": "Manchester"
+    },
+    {
+        "name": "Borussia Dortmund",
+        "year_founded": "1909",
+        "city": "Dortmund"
+    },
+    {
+        "name": "Atletico Madrid",
+        "year_founded": "1903",
+        "city": "Madrid"
+    },
+    {
+        "name": "Olympique Marseille",
+        "year_founded": "1899",
+        "city": "Marseille"
+    },
+    {
+        "name": "Olympique Lyon",
+        "year_founded": "1950",
+        "city": "Lyon"
+    },
+    {
+        "name": "AS Roma",
+        "year_founded": "1927",
+        "city": "Rome"
+    },
+    {
+        "name": "Lazio",
+        "year_founded": "1900",
+        "city": "Rome"
+    },
+    {
+        "name": "Napoli",
+        "year_founded": "1926",
+        "city": "Naples"
+    },
+    {
+        "name": "Fiorentina",
+        "year_founded": "1926",
+        "city": "Florence"
+    },
+    {
+        "name": "Porto",
+        "year_founded": "1893",
+        "city": "Porto"
+    },
+    {
+        "name": "Benfica",
+        "year_founded": "1904",
+        "city": "Lisbon"
+    },
+    {
+        "name": "Sporting CP",
+        "year_founded": "1906",
+        "city": "Lisbon"
+    },
+    {
+        "name": "Paris Saint-Germain",
+        "year_founded": "1970",
+        "city": "Paris"
+    },
+    {
+        "name": "Ajax",
+        "year_founded": "1900",
+        "city": "Amsterdam"
+    },
+    {
+        "name": "PSV",
+        "year_founded": "1913",
+        "city": "Eindhoven"
+    },
+    {
+        "name": "Feyernoord",
+        "year_founded": "1908",
+        "city": "Rotterdam"
+    },
+    {
+        "name": "Boca Juniors",
+        "year_founded": "1905",
+        "city": "Buenos Aires"
+    },
+    {
+        "name": "River Plate",
+        "year_founded": "1901",
+        "city": "Buenos Aires"
+    },
+    {
+        "name": "Flamengo",
+        "year_founded": "1895",
+        "city": "Rio de Janeiro"
+    },
+    {
+        "name": "São Paulo",
+        "year_founded": "1930",
+        "city": "São Paulo"
+    },
+    {
+        "name": "Corinthians",
+        "year_founded": "1910",
+        "city": "São Paulo"
+    },
+    {
+        "name": "Palmeiras",
+        "year_founded": "1914",
+        "city": "São Paulo"
+    },
+    {
+        "name": "Santos",
+        "year_founded": "1912",
+        "city": "Santos"
+    },
+    {
+        "name": "Nacional",
+        "year_founded": "1899",
+        "city": "Montevideo"
+    },
+    {
+        "name": "Peñarol",
+        "year_founded": "1891",
+        "city": "Montevideo"
+    },
+    {
+        "name": "Independiente",
+        "year_founded": "1904",
+        "city": "Avellaneda"
+    },
+    {
+        "name": "Racing Club",
+        "year_founded": "1903",
+        "city": "Avellaneda"
+    },
+    {
+        "name": "Colo-Colo",
+        "year_founded": "1925",
+        "city": "Santiago"
+    },
+    {
+        "name": "Universidad de Chile",
+        "year_founded": "1927",
+        "city": "Santiago"
+    },
+    {
+        "name": "Alianza Lima",
+        "year_founded": "1901",
+        "city": "Lima"
+    },
+    {
+        "name": "Al-Hilal FC",
+        "year_founded": "1957",
+        "city": "Riyadh"
+    },
+    {
+        "name": "Al-Ahli SC",
+        "year_founded": "1937",
+        "city": "Jeddah"
+    },
+    {
+        "name": "Al-Sadd SC",
+        "year_founded": "1969",
+        "city": "Doha"
+    },
+    {
+        "name": "Persepolis FC",
+        "year_founded": "1963",
+        "city": "Tehran"
+    },
+    {
+        "name": "Esteghlal FC",
+        "year_founded": "1945",
+        "city": "Tehran"
+    },
+    {
+        "name": "Urawa Red Diamonds",
+        "year_founded": "1950",
+        "city": "Saitama"
+    },
+    {
+        "name": "Gamba Osaka",
+        "year_founded": "1980",
+        "city": "Osaka"
+    },
+    {
+        "name": "Jeonbuk Hyundai Motors",
+        "year_founded": "1994",
+        "city": "Jeonju"
+    },
+    {
+        "name": "Al-Nassr FC",
+        "year_founded": "1955",
+        "city": "Riyadh"
+    },
+    {
+        "name": "Al-Ittihad FC",
+        "year_founded": "1927",
+        "city": "Jeddah"
+    },
+    {
+        "name": "FC Seoul",
+        "year_founded": "1983",
+        "city": "Seoul"
+    },
+    {
+        "name": "Pohang Steelers",
+        "year_founded": "1973",       
+        "city": "Pohang"
+    },
+    {   "name": "Shanghai SIPG FC", 
+        "year_founded": "2005",       
+        "city": "Shanghai"
+    },
+    {   "name": "Guangzhou Evergrande FC",
+        "year_founded": "1954", 
+        "city": "Guangzhou"  
+    }    
+]
+
+import random
+print("Welcome to the football clubs edition of the Higher-Lower game! ")
+print("Guess which football club was founded earlier!")
+r_club1=random.choice(clubs)
+r_club2=random.choice(clubs)
+r_clubn=random.choice
+score=0
+
+def club_a():
+    global r_club1
+    print(f"Compare A:{r_club1['name']},from {r_club1['city']} ")
+
+def club_b():
+    global r_club2
+    print(f"Compare B:{r_club2['name']},from {r_club2['city']} ")
+
+   
+game_over=False
+def choose():
+    global score
+    global game_over
+    club_a()
+    club_b()
+    c_club=input("Which club do you think was founded earlier? Type A or B").lower()
+    while not game_over:
+        if c_club=='a' and int(r_club1['year_founded'])>int(r_club2['year_founded']) or c_club=='b' and int(r_club2['year_founded'])>int(r_club1['year_founded']):
+           score+=1
+           r_club2=r_club1
+        club_a()
+        club_b()
+        c_club=input("Which club do you think was founded earlier? Type A or B").lower()
+        print(f"Your score is {score}")
+    if game_over==True:
+        print("You lose!")
+
+    
+choose()
+    
+
+
+
+
+
+    
